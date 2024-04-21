e69e8ea305cb8cbb27c5eb865fcf092bed93b9d0
coded15 <91874598+coded15@users.noreply.github.com>
2024-04-21 11:51:35 +0530

    Add project files.

---
+++ addin/AddIn.cs
@@ -0,0 +1,15 @@
+﻿using CodeStack.SwEx.AddIn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addin
+{
+    [ComVisible(true)]
+    public class AddIn : SwAddInEx
+    {
+    }
+}
