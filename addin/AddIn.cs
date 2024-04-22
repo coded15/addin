bbe5afee176c828fe7d63385f9bf0a7a61af3a1e
coded15 <91874598+coded15@users.noreply.github.com>
2024-04-22 19:49:13 +0530

    added proper comments

--- addin/AddIn.cs
+++ addin/AddIn.cs
@@ -3,6 +3,7 @@
 //last commit : 19-04-24, 5:31PM
 //**********************
 
+// Importing required namespaces
 using addin.Properties;
 using CodeStack.SwEx.AddIn;
 using CodeStack.SwEx.AddIn.Attributes;
@@ -20,10 +21,14 @@ using System.Threading.Tasks;
 
 namespace addin
 {
+    // SolidWorks add-in class definition
+    // Indicates that the class is visible to COM components
+    // Specifies a unique identifier (GUID) for the class. This GUID is used to uniquely identify the add-in in the COM registry
     [ComVisible(true), Guid("611C5832-CB81-4122-AC78-954B26ABDA26")]
     [AutoRegister("Slicing", "Generates Slices")] //automatically registers solidworks addin in the registry, it could be recognized and loaded automatically
     public class AddIn : SwAddInEx
     {
+        // Enum to define add-in commands
         [Title("Slicing")]
         [Description("Generates Slices")]
         [Icon(typeof(Resources), nameof(Resources.slices))]
@@ -36,11 +41,15 @@ namespace addin
             GenerateSlices
         }
 
+        // Method called when the add-in is connected to SolidWorks
         public override bool OnConnect()
         {
+            // Add command group to SolidWorks command manager
             AddCommandGroup<Commands_e>(OnButtonClick);
             return true;
         }
+
+        // Method to handle button click events
         private void OnButtonClick(Commands_e cmd)
         {
             try
@@ -54,6 +63,7 @@ namespace addin
             }
             catch (Exception ex)
             {
+                // Display error message to the user
                 App.SendMsgToUser2(ex.Message, (int)swMessageBoxIcon_e.swMbStop, (int)swMessageBoxBtn_e.swMbOk);
             }
         }
@@ -67,20 +77,24 @@ namespace addin
                 if (System.IO.File.Exists(macroPath))
                 {
                     int macroError;
+                    // Run the macro
                     swApp.RunMacro2(macroPath, "", "main", (int)swRunMacroOption_e.swRunMacroDefault, out macroError);
 
                     if (macroError != 0)
                     {
+                        // Throw exception if macro execution fails
                         throw new Exception($"Macro execution failed with error code: {macroError}");
                     }
                 }
                 else
                 {
+                    // Throw exception if macro file not found
                     throw new Exception($"Macro file not found at {macroPath}");
                 }
             }
             else
             {
+                // Throw exception if SolidWorks application object retrieval fails
                 throw new Exception("Failed to retrieve SolidWorks application object");
             }
         }
